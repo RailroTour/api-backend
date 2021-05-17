2e84cd1d2889df06d3a226af8f6ab2981389bea9
cartoonpoet <38898759+cartoonpoet@users.noreply.github.com>
2021-05-18 03:24:50 +0900

    로그인 버그 수정

--- WebContent/js/kakaologin.js
+++ WebContent/js/kakaologin.js
@@ -19,26 +19,24 @@ function loginWithKakao() {
         console.log(authObj["access_token"]);
         $.cookie('access_token', authObj["access_token"], {expires: 7199, path:'/'});
         $.cookie('refresh_token', authObj["refresh_token"], {expires: 5183999});
-
-                var param = {'access':'123'};
-
+        location.href = './oauth/login';
+/*
                 $.ajax({
-                  url:'/backend/member', //request 보낼 서버의 경로
-                  type:'delete', // 메소드(get, post, put 등)
-                  async: false, //동기: false, 비동기(기본값): ture
-                  data:JSON.stringify(param), //보낼 데이터,
+                  url:'./oauth/login', //request 보낼 서버의 경로
+                  type:'post', // 메소드(get, post, put 등)
                   contentType:'application/json; charset=utf-8',
                   success: function(data) {
-                    console.log("data:"+data);
-                    console.log(data.status);
+                    console.log("data:"+JSON.stringify(data));
+					location.href = './index.jsp';
                   },
                   error: function(request, status, error) {
                     //서버로부터 응답이 정상적으로 처리되지 못햇을 때 실행
-                    rtnmsg = JSON.parse(request.responseText);
+                    //rtnmsg = JSON.parse(request.responseText);
                     //alert(rtnmsg.message);
                     console.log("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
                   }
                 });
+                */
       },
       fail: function(err) {
         alert(JSON.stringify(err))
