f6733812e0754c99df04791b00fad6fffe90ba09
somm12 <thals8744@naver.com>
2021-05-21 16:04:42 +0900

    user doGet&Put+doPost&Delete 변경
    http 요청방식 변경

--- src/user/controller/UserController.java
+++ src/user/controller/UserController.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.jasper.tagplugins.jstl.core.If;
+
 import com.oreilly.servlet.MultipartRequest;
 import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
@@ -19,60 +21,46 @@ import user.action.UserDeleteAction;
 import user.action.UserGetAction;
 import user.action.UserPutAction;
 
-@WebServlet("/api/user")
+@WebServlet("/api/user/*")
 public class UserController extends HttpServlet {
 	Action action;
-
+	//get, put
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String servletpath = request.getServletPath();
 		request.setCharacterEncoding("utf-8");
-		action = new UserGetAction();
+		if (servletpath == "/api/user/get") {
+			action = new UserGetAction();
+			System.out.println("get");
+		} else if (servletpath == "/api/user/put") {
+			// 파일 경로 저장.
+			ServletContext context = getServletContext(); // 어플리케이션에 대한 정보를 ServletContext 객체가 갖게 됨.
+			String saveDir = context.getRealPath(""); // 절대경로를 가져옴
+			int maxsize = 3 * 1024 * 1024;// 3MB
+			String encoding = "utf-8";
+			MultipartRequest multi = new MultipartRequest(request, saveDir, maxsize, encoding,
+					new DefaultFileRenamePolicy());
+			System.out.println("절대경로 >> " + saveDir);
+			String file = multi.getFilesystemName("profile_img");
+			request.setAttribute("profile_img", saveDir + file);
+			//
+			action = new UserPutAction();
+			System.out.println("put");
+		}
 		action.execute(request, response);
-		System.out.println("get");
 	}
-
+	//post, delete
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String servletpath = request.getServletPath();
 		request.setCharacterEncoding("utf-8");
-		// 파일 경로 저장.
-		ServletContext context = getServletContext(); // 어플리케이션에 대한 정보를 ServletContext 객체가 갖게 됨.
-		String saveDir = context.getRealPath(""); // 절대경로를 가져옴
-		int maxsize = 3 * 1024 * 1024;// 3MB
-		String encoding = "euc-kr";
-		MultipartRequest multi = new MultipartRequest(request, saveDir, maxsize, encoding,
-				new DefaultFileRenamePolicy());
-		System.out.println("절대경로 >> " + saveDir);
-		String file = multi.getFilesystemName("profile_img");
-		System.out.println("whole path: "+saveDir +file);
-		request.setAttribute("profile_img", saveDir + file);
-		//
-		action = new UserAddAction();
+		if (servletpath == "/api/user/post") {
+			action = new UserAddAction();
+			System.out.println("post");
+		}
+		else if(servletpath == "/api/user/delete") {
+			action = new UserDeleteAction();
+			System.out.println("delete");
+		}
 		action.execute(request, response);
-		System.out.println("post");
 	}
 
-	public void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-
-		// 파일 경로 저장.
-		ServletContext context = getServletContext(); // 어플리케이션에 대한 정보를 ServletContext 객체가 갖게 됨.
-		String saveDir = context.getRealPath(""); // 절대경로를 가져옴
-		int maxsize = 3 * 1024 * 1024;// 3MB
-		String encoding = "euc-kr";
-		MultipartRequest multi = new MultipartRequest(request, saveDir, maxsize, encoding,
-				new DefaultFileRenamePolicy());
-		System.out.println("절대경로 >> " + saveDir);
-		String file = multi.getFilesystemName("profile_img");
-		request.setAttribute("profile_img", saveDir + file);
-		//
-		action = new UserPutAction();
-		action.execute(request, response);
-		System.out.println("put");
-	}
-
-	public void doDelete(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		request.setCharacterEncoding("utf-8");
-		action = new UserDeleteAction();
-		action.execute(request, response);
-		System.out.println("delete");
-	}
 }
